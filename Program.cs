4c8f7d473c19ee36a9a6a86441914fc0ae22bb9d
kai <kaidizhang@byui.edu>
2022-04-27 09:56:43 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unit1Prove // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
